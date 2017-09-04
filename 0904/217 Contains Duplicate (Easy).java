54fdbe43e5d736dbf46e5bea42eec53ca18579f4
Peiyu Wu <pw329@mail.missouri.edu>
2017-09-04 20:30:32 +0800

    0904(...)

---
+++ 0904/217 Contains Duplicate (Easy).java
@@ -0,0 +1,10 @@
+    public boolean containsDuplicate(int[] nums) {
+
+        Arrays.sort(nums);
+        for(int ind = 1; ind < nums.length; ind++) {
+            if(nums[ind] == nums[ind - 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
